5def72369e42496771a2582763cbc657bea0694e
zarinaomor <zarinaomorova@yahoo.com>
2019-03-22 15:39:36 -0700

    Commit C. Booleans

--- js/app.js
+++ js/app.js
@@ -38,4 +38,23 @@ const yourName = "Zarina";
 // console.log('Hello, my name is ' + yourName);
 
 
+// C. Booleans
 
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+//   console.log(a < b);
+//   console.log(c >= d);
+//   console.log('Name' === 'Name');
+//   console.log(true > false);
+//   console.log(((false && false) === (false || false)) || (false !== (false || true)));
+//   console.log(false >= false);
+//   console.log(e === 'Kevin');
+//   console.log(a <= b !== c);
+//   console.log(a === a !== d);
+//   console.log(48 >='48');
