aaedbeaa63a74497e89a6b78667fda3167f1d56b
zarinaomor <zarinaomorova@yahoo.com>
2019-03-22 15:18:59 -0700

    Commit Var & Datatypes Q&A

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// A. Q + A
+
+// How do we assign a value to a variable?
+// The value is assigned with the '=' operator for const, let or var
+
+// How do we change the value of a variable?
+// To change the value of the operator, we access the name of the operator and give it a value. except const. Which can't be redefined
+
+// How do we assign an existing variable to a new variable?
+
+// Remind me, what are declare, assign, and define?
+
+// What is pseudocoding and why should you do it?
+// pseudocoding is the thinking process of the problem before to strat coding. F.e. to describe the steps to solve the problem with a regular language, then translate it to JS 
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+
+// B. Strings
+
+// Create a variable called firstVariable.
+ let firstVariable = "Hello World";
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+firstVariable = 100;
+// Store the value of firstVariable in a new variable called secondVariable
+let secondVariable = firstVariable;
+// Change the value of secondVariable to any string.
+secondVariable = 'Beautiful Day!'
+// What is the value of firstVariable?
+// Beautiful Day!
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+const yourName = "Zarina";
+console.log('Hello, my name is ' + yourName);
+
