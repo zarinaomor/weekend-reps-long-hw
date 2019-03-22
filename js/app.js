ce507c422a86ab1fe4c57fa215221a209c03ccc8
zarinaomor <zarinaomorova@yahoo.com>
2019-03-22 16:00:19 -0700

    Commit section A for Loops

--- js/app.js
+++ js/app.js
@@ -85,3 +85,19 @@ if(personAge >= 16) {
 }
 // If the age is younger than 16, a message should print "Sorry, you're too young."
 
+// II. Loops
+
+// A. The basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive.
+for (let i=0; i<=10; i++) {
+    // console.log(i);
+}
+// Write a loop that will print out all the numbers from 10 up to and including 400.
+for (let i=10; i<=400; i++) {
+    // console.log(i);
+}
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000.
+for (let i=0; i<4000; i++) {
+    console.log(i);
+}
+
