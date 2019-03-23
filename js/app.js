58b35bf490535525107b31e75625d4b45198689d
zarinaomor <zarinaomorova@yahoo.com>
2019-03-22 23:42:37 -0700

    Commit biggie small exercise

--- js/app.js
+++ js/app.js
@@ -258,3 +258,22 @@ myArray.splice(4);
 myArray.reverse();
 
 // console.log(myArray);
+
+// F. Biggie Smalls
+
+// Create a variable that contains an integer.
+ 
+const integer = 10.2;
+
+// Write an if ... else statement that:
+
+if(integer < 100) {
+    console.log('little number');
+} else if(integer >= 100) {
+    console.log('big number');
+}
+
+// console.log()s "little number" if the number is entered is less than 100
+
+// console.log()s big number if the number is greater than or equal to 100.
+
